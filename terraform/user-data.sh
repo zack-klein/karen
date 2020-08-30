f38688ef117c7254b54efdf43f9249be08f954a0
zack-klein <klein.zachary.j@gmail.com>
2020-08-30 14:47:53 -0700

    feat: Add more coming soon ticker and update user-data script

--- terraform/user-data.sh
+++ terraform/user-data.sh
@@ -10,21 +10,30 @@ sudo pip3 install supervisor
 BUCKET=fantasy-football-streamlit
 REQUIREMENTS_KEY=requirements.txt
 APP_KEY=app.py
+LOCAL_BASE_PATH="/home/ec2-user"
+LOCAL_REQUIREMENTS_PATH="$LOCAL_BASE_PATH/$REQUIREMENTS_KEY"
+LOCAL_APP_PATH="$LOCAL_BASE_PATH/$APP_KEY"
 
 # Copy down requirments and app
-sudo aws s3 cp s3://$BUCKET/$REQUIREMENTS_KEY ./$REQUIREMENTS_KEY
-sudo aws s3 cp s3://$BUCKET/$APP_KEY ./$APP_KEY
+sudo aws s3 cp s3://$BUCKET/$REQUIREMENTS_KEY $LOCAL_REQUIREMENTS_PATH
+sudo aws s3 cp s3://$BUCKET/$APP_KEY $LOCAL_APP_PATH
 
 # Install the requirements...
-sudo pip3 install -r $REQUIREMENTS_KEY
+sudo pip3 install -r $LOCAL_REQUIREMENTS_PATH
 STREAMLIT_PATH=$(which streamlit)
-STREAMLIT_START_COMMAND="sudo $STREAMLIT_PATH run $APP_KEY --server.port 80"
+STREAMLIT_START_COMMAND="sudo $STREAMLIT_PATH run $LOCAL_APP_PATH --server.port 80"
 
 # Set up supervisor to run the app in the background...
 SUPERVISOR_PATH=$(which supervisord)
-SUPERVISOR_LOG_FOLDER="/var/log/supervisord/"
-SUPERVISOR_CONF_PATH="/home/ec2-user/supervisord.conf"
+SUPERVISOR_LOG_FOLDER="$LOCAL_BASE_PATH/supervisord/"
+SUPERVISOR_CONF_PATH="$LOCAL_BASE_PATH/supervisord.conf"
 sudo mkdir -p $SUPERVISOR_LOG_FOLDER
 printf "[supervisord]\nlogfile=$SUPERVISOR_LOG_FOLDER/supervisord.log\n\n" >> $SUPERVISOR_CONF_PATH
 printf "[program:streamlit]\ncommand=$STREAMLIT_START_COMMAND" >> $SUPERVISOR_CONF_PATH
 sudo $SUPERVISOR_PATH -c $SUPERVISOR_CONF_PATH
+
+# Set up a cron job so we don't need to tear down the instance to get a new version of the code
+LOCAL_CRON_PATH="$LOCAL_BASE_PATH/sync_app.sh"
+LOCAL_CRON_LOG_PATH="$LOCAL_BASE_PATH/log_sync_app.log"
+echo "* * * * * aws s3 sync s3://$BUCKET/$APP_KEY $LOCAL_APP_PATH --exact-timestamps >> $LOCAL_CRON_LOG_PATH" >> $LOCAL_CRON_PATH
+crontab $LOCAL_CRON_PATH
