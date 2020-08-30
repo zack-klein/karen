341d732e93952690e5704de6b69ec226d9be1f96
zack-klein <klein.zachary.j@gmail.com>
2020-08-30 09:58:48 -0700

    fix: Add terraform fmt to pre-commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,12 +13,12 @@ provider "aws" {
 }
 
 module "instance" {
-  source         = "github.com/zack-klein/ec2-instance"
-  instance_name  = "fantasy-football"
-  user_data_path = "./user-data.sh"
-  vpc_id         = "vpc-792b4b03"
-  subnets        = ["subnet-bc9fcee0", "subnet-21237746", "subnet-7b91c255"]
-  ssh_public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBIu48ryZCZZlufVF9AgjBrEmjau3Hmb20Aah75SYQTAhA5+9E5MuXJ1UwlDBXUPDIv/VFgu+/AmyT1kk1+8N9OE8I2kVcr8nAsFV7c7PNG60zxMXtCiCA8KgzQw/JeUceCmSZnGTCY5pgkXFvTAVaPtNywaEuKU9sICi+J+jm6WBk+IbX+GQYCJCTjGugbH0SOqivyBj/xiVgXQAdDKybvSQkWZqlERnfpbI0vSGEVqzlia+D54TlxaL5tnI76KoR6toWBmp81Y4JVHNAMW7LMerbQuor9gEUWWY1pdY/NikY9Sy1YMPOiYtAqbi/x3rm03tsr1mEAsu7o29g+oID"
+  source           = "github.com/zack-klein/ec2-instance"
+  instance_name    = "fantasy-football"
+  user_data_path   = "./user-data.sh"
+  vpc_id           = "vpc-792b4b03"
+  subnets          = ["subnet-bc9fcee0", "subnet-21237746", "subnet-7b91c255"]
+  ssh_public_key   = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCBIu48ryZCZZlufVF9AgjBrEmjau3Hmb20Aah75SYQTAhA5+9E5MuXJ1UwlDBXUPDIv/VFgu+/AmyT1kk1+8N9OE8I2kVcr8nAsFV7c7PNG60zxMXtCiCA8KgzQw/JeUceCmSZnGTCY5pgkXFvTAVaPtNywaEuKU9sICi+J+jm6WBk+IbX+GQYCJCTjGugbH0SOqivyBj/xiVgXQAdDKybvSQkWZqlERnfpbI0vSGEVqzlia+D54TlxaL5tnI76KoR6toWBmp81Y4JVHNAMW7LMerbQuor9gEUWWY1pdY/NikY9Sy1YMPOiYtAqbi/x3rm03tsr1mEAsu7o29g+oID"
   instance_profile = aws_iam_instance_profile.profile.name
 }
 
