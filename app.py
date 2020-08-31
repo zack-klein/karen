797df6cd32243ad2d1d5f84d4a66bb5949727b93
zack-klein <klein.zachary.j@gmail.com>
2020-08-31 08:11:17 -0700

    feat: Add team luck analysis

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import json
 import boto3
 import pandas as pd
 import streamlit as st
+import plotly.graph_objects as go
 
 from espn_api.football import League
 
@@ -68,7 +69,7 @@ def build_power_rankings_df(power_rankings):
     return df
 
 
-def build_team_df(team, current_week):
+def build_score_df(team, current_week):
     """
     Build a nice clean df of a given team's data.
 
@@ -87,6 +88,159 @@ def build_team_df(team, current_week):
     team_df = pd.DataFrame(team_data, columns=columns)
     team_df.set_index("Week", inplace=True)
     return team_df
+
+
+def get_projected_points(lineup):
+    """
+    Build projected points from a lineup.
+    """
+    projected = 0
+
+    for player in lineup:
+
+        if player.slot_position != "BE":
+            projected += player.projected_points
+
+    return projected
+
+
+def build_team_df(team, current_week, league):
+    """
+    Build a nice clean df of a given team's data.
+
+    :param Team team: Team object.
+    """
+    columns = [
+        "Index",
+        "Week",
+        "Projected Points",
+        "Points Scored",
+        "Projection Diff",
+        "Opponent Projected Score",
+        "Projected Result",
+        "Result",
+        "Lost/Won By",
+        "Opponent",
+        "Power Ranking",
+        "Power Ranking Score",
+    ]
+    team_data = []
+
+    bar = st.progress(0.0)
+
+    for i in range(1, current_week + 1):
+
+        print(f"Evaluating week {i}...")
+        progress = i / len([i for i in range(current_week)])
+        bar.progress(progress)
+
+        score = team.scores[i - 1]
+        diff = team.mov[i - 1]
+        result = "W" if diff > 0 else "L"
+        opponent = team.schedule[i - 1].team_name
+
+        # Get the ranking for the current week
+        rankings = league.power_rankings(week=i)
+        for j, ranking in enumerate(rankings):
+            ranked_team = ranking[1]
+
+            if ranked_team.team_id == team.team_id:
+                # league_ranking = ranked_team.standing
+                power_ranking = j
+                power_ranking_score = ranking[0]
+
+        # Get the projected total
+        box_scores = league.box_scores(week=i)
+        for k, box_score in enumerate(box_scores):
+            # Figure out if this team was home or away
+            if box_score.away_team == 0:
+                box_score.away_team = box_score.home_team
+
+            elif box_score.home_team == 0:
+                box_score.home_team = box_score.away_team
+
+            if box_score.away_team.team_id == team.team_id:
+                projected = get_projected_points(box_score.away_lineup)
+                opp_projected = get_projected_points(box_score.home_lineup)
+
+                if projected > opp_projected:
+                    projected_result = "W"
+                elif projected < opp_projected:
+                    projected_result = "L"
+                else:
+                    projected_result = "T"
+
+                break
+            elif box_score.home_team.team_id == team.team_id:
+                projected = get_projected_points(box_score.home_lineup)
+                opp_projected = get_projected_points(box_score.away_lineup)
+
+                if projected > opp_projected:
+                    projected_result = "W"
+                elif projected < opp_projected:
+                    projected_result = "L"
+                else:
+                    projected_result = "T"
+
+                break
+            else:
+                pass
+
+        row = [
+            "",
+            i,
+            projected,
+            score,
+            score - projected,
+            opp_projected,
+            projected_result,
+            result,
+            diff,
+            opponent,
+            power_ranking,
+            power_ranking_score,
+        ]
+        team_data.append(row)
+
+    team_df = pd.DataFrame(team_data, columns=columns)
+    team_df.set_index("Index", inplace=True)
+    return team_df
+
+
+def build_projected_vs_actual_chart(df):
+    """
+    Build a plotly chart of Projected vs. Actual points
+    that highlights unforeseen outcomes.
+    """
+
+    fig = go.Figure()
+    fig.add_trace(
+        go.Scatter(
+            x=df["Week"],
+            y=df["Projected Points"],
+            fill="tozeroy",
+            name="Projected Points",
+            text=df["Projected Result"],
+        )
+    )
+    fig.add_trace(
+        go.Scatter(
+            x=df["Week"],
+            y=df["Points Scored"],
+            fill="tonexty",
+            name="Actual Points",
+            text=df["Result"],
+        )
+    )
+
+    for i, row in df.iterrows():
+        week = row["Week"]
+        proj_result = row["Projected Result"]
+        act_result = row["Result"]
+        if proj_result != act_result:
+
+            fig.add_annotation(x=week, y=row["Points Scored"], text=act_result)
+    return fig
 
 
 LOGO_URL = "https://vignette.wikia.nocookie.net/spongebob/images/1/18/Karen-blue-form-stock-art.png/revision/latest?cb=20200317150606"  # noqa:E501
@@ -155,13 +309,38 @@ st.write("## Team Journeys")
 TEAMS = [t.team_name for t in league.teams]
 team_name = st.selectbox("Team:", TEAMS)
 team = [t for t in league.teams if t.team_name == team_name][0]
-st.write(
-    f"### {team.team_name} is on a {team.streak_length} game {team.streak_type.lower()} streak."  # noqa:E501
-)
+st.write(f"### {team.team_name} ({team.wins}-{team.losses})")  # noqa:E501
+# with st.spinner(f"Crunching {team.team_name}'s numbers... hang tight!"):
+score_df = build_score_df(team, league.current_week)
+team_df = build_team_df(team, league.current_week, league)
 
-with st.spinner(f"Crunching {team.team_name}'s numbers... hang tight!"):
-    team_df = build_team_df(team, league.current_week)
+# Build a paragraph of analysis.
+unexpected_df = team_df[team_df["Projected Result"] != team_df["Result"]]
+unexpected_outcomes = len(unexpected_df)
+unexpected_wins = len(unexpected_df[unexpected_df["Result"] == "W"])
+unexpected_losses = len(unexpected_df[unexpected_df["Result"] == "L"])
 
-st.area_chart(team_df)
+if unexpected_wins > unexpected_losses:
+    favored = "helped"
 
-st.write("More analysis coming soon!")
+elif unexpected_wins < unexpected_losses:
+    favored = "hurt"
+
+else:
+    favored = "neither helped nor hurt"
+
+TEXT = f"""
+    {team.team_name} currently has a record of **{team.wins}** wins and
+    **{team.losses}** losses. **{unexpected_outcomes}** of these
+    {team.wins + team.losses} outcomes can be considered unexpected
+    (the actual result was different than the projected result), with
+    **{unexpected_wins}** unexpected wins and **{unexpected_losses}**
+    unexpected losses.
+
+    This team has generally been **{favored}** by unexpected outcomes this
+    season.
+"""
+st.write(TEXT)
+fig = build_projected_vs_actual_chart(team_df)
+
+st.write(fig)
