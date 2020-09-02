16786c7b7888b29a4d567a5e735bf858517679c9
zack-klein <klein.zachary.j@gmail.com>
2020-09-02 13:01:26 -0700

    ci: Add python3 -m prefix to ci commands

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,9 +2,9 @@ set -ex
 
 echo "Running formatting tests..."
 
-black . --check --line-length 79
-flake8
-bandit . -r -lll
+python3 -m black . --check --line-length 79
+python3 -m flake8
+python3 -m bandit . -r -lll
 
 echo "Running pytests..."
 python3 -m pytest -s
